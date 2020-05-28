934edd591879a49a53dd3305601823b25f90dc57
Mehmet Eyüpoğlu <mehmeteyupoglu2@gmail.com>
2020-05-28 22:04:19 +0300

    Refactor q2

--- q2.js
+++ q2.js
@@ -6,13 +6,21 @@ function asyncItemListing(arr) {
     for(let i = 0; i < arr.length; i++){
       setTimeout(() => console.log(arr[i]), (i+1) * 1000)
     }
+    
   }
+
+function asyncItemListingWithMap(arr) {
+  arr.map((item, index) => {
+    return setTimeout(() => console.log(item), (index + 1) * 1000)
+  })
+}
 
 //Author's note: The question implies that each element has its index num + 1 sec and that way the function could yield the expected result. 
                 //In case of any misunderstanding, I kindly ask you to ask for a revision. 
+                //Both functions work and do the same job. 
   
 const a = [1,2,3,4,5,1,4,3,6,8,2]
-asyncItemListing(a)
+asyncItemListingWithMap(a)
 
-// const b = ["a", "b", "c", "d"]
-// asyncItemListing(b)
+const b = ["a", "b", "c", "d"]
+asyncItemListing(b)
